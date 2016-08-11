d85a288e83118521758c91940bc64613c4fda61a
spenceryoung <syoung42788@gmail.com>
2016-08-11 00:30:53 -0500

    url hashing

--- js/main.js
+++ js/main.js
@@ -3,28 +3,33 @@ var nodeList = document.getElementsByClassName('link'),
 
 (function() {
     var xhr = new XMLHttpRequest();
+        landUrl = nodeList[0].getAttribute('href');
     xhr.onload = function() {
-        if(xhr.status === 200) {
+        if (xhr.status === 200) {
             content.innerHTML = xhr.responseText;
         }
+        document.location.hash = landUrl;
     };
-    xhr.open('GET', 'partials/one.html', true);
+    xhr.open('GET', landUrl, true);
     xhr.send(null);
 }());
 
-for (var i=0; i<nodeList.length; i++) {
+for (var i = 0; i < nodeList.length; i++) {
     nodeList[i].addEventListener('click', function(e) {
         e.preventDefault();
-        var url = this.href;
+        var url = this.getAttribute('href'),
+            fullUrl = document.location,
             xhr = new XMLHttpRequest();
         document.getElementsByClassName('active-tab')[0].classList.remove('active-tab');
         this.className += " " + "active-tab";
         xhr.onload = function() {
-            if(xhr.status === 200) {
+            if (xhr.status === 200) {
                 content.innerHTML = xhr.responseText;
             }
         };
         xhr.open('GET', url, true);
         xhr.send(null);
+        document.location.hash = "/" + url;
+        window.scrollTo(0, 0);
     }, false);
 }
